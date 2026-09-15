b988bd48f3a09978d3350662abf3953b77af1cdd
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-09-15 12:13:19 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWrphU0Ixnwwf1EvzvmCi_RbMYm_tTnj8",
+  authDomain: "civiclens-f6fa7.firebaseapp.com",
+  projectId: "civiclens-f6fa7",
+  storageBucket: "civiclens-f6fa7.firebasestorage.app",
+  messagingSenderId: "11523268542",
+  appId: "1:11523268542:web:378b17458c162f9bd0a3ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
