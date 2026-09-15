91d6a990d6893a9863954cbe66c8c761e060dc71
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-09-15 13:10:50 +0530

    Update my-reports.js

--- js/my-reports.js
+++ js/my-reports.js
@@ -1,1 +1,8 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./firebase.js";
 
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        window.location.href = "login.html";
+    }
+});
