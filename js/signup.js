0c8c9273f38539110eb3f3a9f01249860fb2b1fa
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-09-15 12:24:46 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,22 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./firebase.js";
 
+const signupForm = document.querySelector(".auth-form");
+
+signupForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Account created successfully!");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
