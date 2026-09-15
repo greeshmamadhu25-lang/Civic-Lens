eba2f85061bb6300a10c721d624366c69e974321
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-09-15 12:41:57 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCWrphU0Ixnwwf1EvzvmCi_RbMYm_tTnj8",
   authDomain: "civiclens-f6fa7.firebaseapp.com",
@@ -12,11 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "11523268542",
   appId: "1:11523268542:web:378b17458c162f9bd0a3ef"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const app = initializeApp(firebaseConfig);
 
