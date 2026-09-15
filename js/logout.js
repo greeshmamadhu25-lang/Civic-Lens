671daa92b60d2e5d2a29d54bcb993090b617e6d8
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-09-15 13:04:36 +0530

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,1 +1,15 @@
+import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./firebase.js";
 
+const logoutButton = document.getElementById("logout");
+
+if (logoutButton) {
+    logoutButton.addEventListener("click", async () => {
+        try {
+            await signOut(auth);
+            window.location.href = "login.html";
+        } catch (error) {
+            alert(error.message);
+        }
+    });
+}
